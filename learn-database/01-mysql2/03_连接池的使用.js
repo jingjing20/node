ce07feb38544e603689887f23da6a861be285e70
jingjing20 <1269928993@qq.com>
2021-02-15 22:23:01 +0800

    mysql2 使用

---
+++ learn-database/01-mysql2/03_连接池的使用.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+// 1.创建连接池
+const connections = mysql.createPool({
+	host: 'localhost',
+	port: 3306,
+	database: 'coderhub',
+	user: 'root',
+	password: '19720928mm',
+	connectionLimit: 10,
+});
+
+// 2.使用连接池
+const statement = `
+  SELECT * FROM products WHERE price > ? AND score > ?;
+`;
+connections.execute(statement, [6000, 7], (err, results) => {
+	console.log(results);
+});
---
+++ learn-database/01-mysql2/04_promise的方式.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2');
+
+// 1.创建连接池
+const connections = mysql.createPool({
+	host: 'localhost',
+	port: 3306,
+	database: 'coderhub',
+	user: 'root',
+	password: '19720928mm',
+	connectionLimit: 10,
+});
+
+// 2.使用连接池
+const statement = `
+  SELECT * FROM products WHERE price > ? AND score > ?;
+`;
+connections
+	.promise()
+	.execute(statement, [6000, 7])
+	.then(([results]) => {
+		console.log(results);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
